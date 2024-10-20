c795b421f2cbf9e9d9bdbcd3ea472fcac62791a2
Md. Fazley Rabbi <fazley111@gmail.com>
2024-10-20 21:44:49 +0600

    tabler admin template mastering done, dashboard sample view added

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,53 @@
+<!-- resources/views/admin/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Admin Dashboard')</title>
+    <link rel="stylesheet" href="{{ asset('admin/dist/css/tabler.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/dist/css/tabler-flags.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/dist/css/tabler-payments.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/dist/css/tabler-vendors.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/dist/css/demo.min.css') }}">
+    <style>
+        @import url('https://rsms.me/inter/inter.css');
+        :root {
+            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+        }
+        body {
+            font-feature-settings: "cv03", "cv04", "cv11";
+        }
+      </style>
+    @stack('styles')
+</head>
+<body>
+    <!-- Sidebar -->
+    @include('admin.partials.sidebar')
+
+    <!-- Main content -->
+    <div class="page">
+        <!-- Navbar -->
+        @include('admin.partials.navbar')
+
+        <!-- Page content -->
+        <div class="page-wrapper">
+            @yield('content')
+        </div>
+
+        @include('admin.partials.footer')
+    </div>
+
+    <script src="{{ asset('admin/dist/js/demo-theme.js') }}"></script>
+    <!-- Libs JS -->
+    <script src="{{ asset('admin/dist/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('admin/dist/libs/jsvectormap/dist/js/jsvectormap.js') }}"></script>
+    <script src="{{ asset('admin/dist/libs/jsvectormap/dist/maps/world.js') }}"></script>
+    <!-- Tabler Core -->
+    <script src="{{ asset('admin/dist/js/tabler.min.js') }}"></script>
+    <script src="{{ asset('admin/dist/js/demo.min.js') }}"></script>
+
+    @stack('scripts')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return '<h1>Hello World</h1>';
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+// Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function () {
+//     Route::get('/dashboard', [DashboardController::class, 'index'])
+//         ->name('dashboard');
+//     Route::resource('/users', \App\Http\Controllers\Admin\UserController::class);
+// });
