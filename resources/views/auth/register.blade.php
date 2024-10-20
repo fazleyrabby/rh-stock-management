0e76b324e5ebc0499c880075d46b98c49d7dd645
Md. Fazley Rabbi <fazley111@gmail.com>
2024-10-21 00:37:11 +0600

    minor change

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -45,13 +45,13 @@
                                 <span>{{ $message }}</span>
                             @enderror
                         </div>
-                        <d class="mb-3">
+                        <div class="mb-3">
                             <label class="form-label">Email address</label>
                             <input type="email" class="form-control" placeholder="Enter email" name="email">
                             @error('email')
                                 <span>{{ $message }}</span>
                             @enderror
-                        </d
+                        </div>
                         <div class="mb-3">
                             <label class="form-label">Password</label>
                             <div class="input-group input-group-flat">
