77799e0a6220c1f3106f82906a6b02a4b2d50e12
Md. Fazley Rabbi <fazley111@gmail.com>
2024-11-23 12:29:06 +0600

    observer trigger minor update in stockmovement model

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        StockMovement::observe(StockMovementObserver::class);
+        // StockMovement::observe(StockMovementObserver::class);
     }
 }
